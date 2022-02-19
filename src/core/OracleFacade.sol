31e5d09ded595e2bf3c444459f5b6f1263d1e81e
ruvaag <ruvaag@gmail.com>
2022-02-19 18:58:39 +0530

    rename IPriceFeed to IOracle

--- src/core/OracleFacade.sol
+++ src/core/OracleFacade.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import {IOracle} from "./IOracle.sol";
 import {Errors} from "../utils/Errors.sol";
-import {IPriceFeed} from "./IPriceFeed.sol";
 import {Ownable} from "../utils/Ownable.sol";
 
-contract OracleFacade is Ownable, IPriceFeed {
-    address public immutable WETH_ADDR;
+contract OracleFacade is Ownable, IOracle {
+    address public immutable WETH;
 
-    mapping(address => address) public priceFeed;
+    mapping(address => IOracle) public oracle;
 
     event UpdateFeed(address indexed tokenAddr, address indexed feedAddr);
 
     // TODO Should WETH_ADDR be mutable?
-    constructor(address wethAddress) Ownable(msg.sender) {
-        WETH_ADDR = wethAddress;
+    constructor(address weth) Ownable(msg.sender) {
+        WETH = weth;
     }
 
     /// @dev Assume that the response has 18 decimals
     function getPrice(address token) external view returns (uint) {
-        if(token == address(0) || token == WETH_ADDR) return 1e18;
-        if(priceFeed[token] == address(0)) revert Errors.PriceUnavailable();
-        return IPriceFeed(priceFeed[token]).getPrice(token);
+        if(token == address(0) || token == WETH) return 1e18;
+        if(address(oracle[token]) == address(0)) revert Errors.PriceUnavailable();
+        return oracle[token].getPrice(token);
     }
 
      // AdminOnly
-    function setFeed(address token, address _priceFeed) external adminOnly {
-        priceFeed[token] = _priceFeed;
-        emit UpdateFeed(token, priceFeed[token]);
+    function setFeed(address token, IOracle _oracle) external adminOnly {
+        oracle[token] = _oracle;
+        emit UpdateFeed(token, address(_oracle));
     }
 }
