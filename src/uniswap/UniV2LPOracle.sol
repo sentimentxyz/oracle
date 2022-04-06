bd0725d2d32feea5a7161d88a5cfc7b369f7fb6e
Ruvaag <ruvaag@gmail.com>
2022-04-06 07:36:13 +0530

    feat: implement an oracle for UniV2 LP Tokens (#10)
    * feat: implement oracle for UniV2 LP Tokens
    
    * refactor with gm
    
    * rename contract
    
    * gas optimization

---
+++ src/uniswap/UniV2LPOracle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IOracle} from "../core/IOracle.sol";
+import {IUniswapV2Pair} from "./IUniswapV2Pair.sol";
+import {PRBMathUD60x18} from "prb-math/PRBMathUD60x18.sol";
+
+contract UniV2LpOracle is IOracle {
+    using PRBMathUD60x18 for uint;
+
+    IOracle public immutable oracle;
+
+    constructor(IOracle _oracle) {
+        oracle = _oracle;
+    }
+
+    // Adapted from https://blog.alphaventuredao.io/fair-lp-token-pricing
+    function getPrice(address pair) external view returns (uint) {
+        (uint r0, uint r1,) = IUniswapV2Pair(pair).getReserves();
+        
+        // 2 * sqrt(r0 * r1) * sqrt(p0 * p1) / totalSupply
+        return r0
+            .gm(r1)
+            .div(IUniswapV2Pair(pair).totalSupply())
+            .mul(
+                oracle.getPrice(IUniswapV2Pair(pair).token0())
+                .gm(oracle.getPrice(IUniswapV2Pair(pair).token1()))
+            ).mul(2e18);
+    }
+}
