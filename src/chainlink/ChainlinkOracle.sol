06a1ad364cf292b30647524258ef83714194fae5
r0ohafza <coldroohafza@gmail.com>
2022-04-12 09:57:15 -0700

    feat: refactor chainlink oracle to consume latestAnswer()

--- src/chainlink/ChainlinkOracle.sol
+++ src/chainlink/ChainlinkOracle.sol
@@ -6,20 +6,29 @@ import {Ownable} from "../utils/Ownable.sol";
 import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
 
 contract ChainlinkOracle is Ownable, IOracle {
+    
+    AggregatorV3Interface immutable ethPriceFeed;
+
     mapping(address => AggregatorV3Interface) public feed;
 
     event UpdateFeed(address indexed token, address indexed feed);
 
-    constructor() Ownable(msg.sender) {}
+    constructor(AggregatorV3Interface _ethPriceFeed) Ownable(msg.sender) {
+        ethPriceFeed = _ethPriceFeed;
+    }
 
-    /// @dev Assume that the response has 18 decimals
     function getPrice(address token) external view override returns (uint) {
-        (, int price, , ,) = feed[token].latestRoundData();
-        return uint(price);
+        return (
+            uint(feed[token].latestAnswer())*1e10/
+            uint(ethPriceFeed.latestAnswer())*1e10
+        );
     }
 
     // AdminOnly
-    function setFeed(address token, AggregatorV3Interface _feed) external adminOnly {
+    function setFeed(
+        address token,
+        AggregatorV3Interface _feed
+    ) external adminOnly {
         feed[token] = _feed;
         emit UpdateFeed(token, address(_feed));
     }
