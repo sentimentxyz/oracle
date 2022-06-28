2ce2f6a936c81062be38659306cbc74f97f7b63d
r0ohafza <coldroohafza@gmail.com>
2022-06-28 12:24:25 -0700

    doc: core

--- src/core/IOracle.sol
+++ src/core/IOracle.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.15;
 
 interface IOracle {
+
+    /**
+        @notice Fetches price of a given token in terms of ETH
+        @param token Address of token
+        @return price Price of token in terms of ETH
+    */
     function getPrice(address token) external view returns (uint);
 }
--- src/core/OracleFacade.sol
+++ src/core/OracleFacade.sol
@@ -1,24 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.15;
 
 import {IOracle} from "./IOracle.sol";
 import {Errors} from "../utils/Errors.sol";
 import {Ownable} from "../utils/Ownable.sol";
 
+/**
+    @title Oracle Facade
+    @notice This contract acts as a single interface for the client to fetch
+    price of a given token in terms of eth
+*/
 contract OracleFacade is Ownable, IOracle {
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Mapping of token to Price Oracle for the token
     mapping(address => IOracle) public oracle;
 
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Contract Constructor
     constructor() Ownable(msg.sender) {}
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   EVENTS                                   */
+    /* -------------------------------------------------------------------------- */
 
     event UpdateOracle(address indexed token, address indexed feed);
 
-    /// @dev Assume that the response has 18 decimals
+    /* -------------------------------------------------------------------------- */
+    /*                              PUBLIC FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IOracle
     function getPrice(address token) external view returns (uint) {
         if(address(oracle[token]) == address(0)) revert Errors.PriceUnavailable();
         return oracle[token].getPrice(token);
     }
 
-     // AdminOnly
+    /* -------------------------------------------------------------------------- */
+    /*                               ADMIN FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
     function setOracle(address token, IOracle _oracle) external adminOnly {
         oracle[token] = _oracle;
         emit UpdateOracle(token, address(_oracle));
