655954deeffe64ed09f0707a7e85bc06651d6880
r0ohafza <coldroohafza@gmail.com>
2022-06-28 13:07:33 -0700

    doc: WETH Oracle

--- src/weth/WETHOracle.sol
+++ src/weth/WETHOracle.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 import {IOracle} from "../core/IOracle.sol";
 
-/// @dev Used for address(0) and WETH price calls to the Oracle
+/**
+    @title WETH Oracle
+    @notice Price Oracle for WETH
+    @dev Used for address(0) and WETH price calls to the Oracle
+*/
 contract WETHOracle is IOracle {
+
+    /* -------------------------------------------------------------------------- */
+    /*                              PUBLIC FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IOracle
     function getPrice(address) external pure returns (uint) { return 1e18; }
 }
