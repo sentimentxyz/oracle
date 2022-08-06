711d80b86eb9d53203d3409ec421270c55554137
r0ohafza <coldroohafza@gmail.com>
2022-08-06 10:44:45 -0700

    feat: add univ3 twap oracle

--- src/uniswap/library/OracleLibrary.sol
+++ src/uniswap/library/OracleLibrary.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.15;
 
-import './TickMath.sol';
-import '../interface/IUniswapV3Pool.sol';
-import 'solmate/utils/FixedPointMathLib.sol';
+import "./TickMath.sol";
+import "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "solmate/utils/FixedPointMathLib.sol";
 
 /// @title Oracle library
 /// @notice Provides functions to integrate with V3 pool oracle, Forked from
