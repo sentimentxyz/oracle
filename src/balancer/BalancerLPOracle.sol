5c36e99e7f58e6d10b7b9b5e0082ad37b6e2aec1
r0ohafza <coldroohafza@gmail.com>
2022-08-18 13:07:33 -0700

    doc: add docs

--- src/balancer/BalancerLPOracle.sol
+++ src/balancer/BalancerLPOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import "forge-std/console.sol";
 import "src/core/IOracle.sol";
 import "src/utils/IERC20.sol";
 import "./library/FixedPoint.sol";
@@ -16,17 +15,39 @@ interface IVault {
     function getPoolTokens(bytes32) external view returns (address[] memory, uint256[] memory, uint256);
 }
 
+/**
+    @title Balancer LP Oracle for weighted pool
+    @notice Oracle for weighted balancer pool tokens
+    https://dev.balancer.fi/references/lp-tokens/valuing
+*/
 contract WeightedBalancerLPOracle is IOracle {
     using FixedPoint for uint;
 
+    /* -------------------------------------------------------------------------- */
+    /*                              STORAGE VARIABLES                             */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice Balancer vault
     IVault immutable vault;
+
+    /// @notice Oracle Facade
     IOracle immutable oracleFacade;
 
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Contract constructor
+        @param _oracle Oracle Facade
+        @param _vault Balance Vault
+    */
     constructor(IOracle _oracle, IVault _vault) {
         vault = _vault;
         oracleFacade = _oracle;
     }
 
+    /// @inheritdoc IOracle
     function getPrice(address token) external view returns (uint) {
         (
             address[] memory poolTokens,
