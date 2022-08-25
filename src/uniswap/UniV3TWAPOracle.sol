a1aa8f4cf24b93b41465e8a102a06de92b9d20e8
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-08-25 16:53:31 -0700

    SEN-218 | feat: add univ3 twap oracle (#26)
    * feat: add univ3 twap oracle
    
    * forge install: v3-core
    
    * feat: add univ3 twap oracle

---
+++ src/uniswap/UniV3TWAPOracle.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {Errors} from "../utils/Errors.sol";
+import {IOracle} from "../core/IOracle.sol";
+import {Ownable} from "../utils/Ownable.sol";
+import {OracleLibrary} from "./library/OracleLibrary.sol";
+import {IERC20} from "../utils/IERC20.sol";
+
+/**
+    @title Uniswap V3 TWAP Oracle
+    @notice Price oracle that uses univ3 token-weth pools to fetch price of
+    a given token in terms of ETH
+*/
+contract UniV3TWAPOracle is Ownable, IOracle {
+
+    /* -------------------------------------------------------------------------- */
+    /*                               STATE VARIABLES                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @notice TWAP period, default = 1800 seconds
+    uint32 public twapPeriod = 1800;
+
+    /// @notice arbi:WETH
+    address public constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+
+    /// @notice mapping of token to token-WETH Uniswap v3 pool
+    mapping(address => address) poolFor;
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 CONSTRUCTOR                                */
+    /* -------------------------------------------------------------------------- */
+
+    constructor() Ownable(msg.sender) {}
+
+    /* -------------------------------------------------------------------------- */
+    /*                              PUBLIC FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /// @inheritdoc IOracle
+    function getPrice(address token) public view returns (uint256) {
+
+        address pool;
+        if ((pool = poolFor[token]) == address(0)) {
+            revert Errors.PriceUnavailable();
+        }
+
+        (int24 arithmeticMeanTick, ) = OracleLibrary.consult(
+            pool,
+            twapPeriod
+        );
+
+        return OracleLibrary.getQuoteAtTick(
+            arithmeticMeanTick,
+            uint128(10) ** IERC20(token).decimals(),
+            token,
+            WETH
+        );
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                               ADMIN FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+        @notice Update twap period
+        @param _twapPeriod New twap period in seconds
+    */
+    function updateTwapPeriod(uint32 _twapPeriod) external adminOnly {
+        twapPeriod = _twapPeriod;
+    }
+
+    /**
+        @notice Set Uniswap v3 WETH-token pool for a given token
+        @param token Address of token
+        @param pool Address of pool
+    */
+    function setPool(address token, address pool) external adminOnly {
+        poolFor[token] = pool;
+    }
+}
---
+++ src/uniswap/library/OracleLibrary.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.15;
+
+import "./TickMath.sol";
+import "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "solmate/utils/FixedPointMathLib.sol";
+
+/// @title Oracle library
+/// @notice Provides functions to integrate with V3 pool oracle, Forked from
+/// https://github.com/Uniswap/v3-periphery/blob/main/contracts/libraries/OracleLibrary.sol
+library OracleLibrary {
+    /// @notice Calculates time-weighted means of tick and liquidity for a given Uniswap V3 pool
+    /// @param pool Address of the pool that we want to observe
+    /// @param secondsAgo Number of seconds in the past from which to calculate the time-weighted means
+    /// @return arithmeticMeanTick The arithmetic mean tick from (block.timestamp - secondsAgo) to block.timestamp
+    /// @return harmonicMeanLiquidity The harmonic mean liquidity from (block.timestamp - secondsAgo) to block.timestamp
+    function consult(address pool, uint32 secondsAgo)
+        internal
+        view
+        returns (int24 arithmeticMeanTick, uint128 harmonicMeanLiquidity)
+    {
+        require(secondsAgo != 0, 'BP');
+
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = secondsAgo;
+        secondsAgos[1] = 0;
+
+        (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s) =
+            IUniswapV3Pool(pool).observe(secondsAgos);
+
+        int56 tickCumulativesDelta = tickCumulatives[1] - tickCumulatives[0];
+        uint160 secondsPerLiquidityCumulativesDelta =
+            secondsPerLiquidityCumulativeX128s[1] - secondsPerLiquidityCumulativeX128s[0];
+
+        arithmeticMeanTick = int24(tickCumulativesDelta / int32(secondsAgo));
+        // Always round to negative infinity
+        if (tickCumulativesDelta < 0 && (tickCumulativesDelta % int32(secondsAgo) != 0)) arithmeticMeanTick--;
+
+        // We are multiplying here instead of shifting to ensure that harmonicMeanLiquidity doesn't overflow uint128
+        uint192 secondsAgoX160 = uint192(secondsAgo) * type(uint160).max;
+        harmonicMeanLiquidity = uint128(secondsAgoX160 / (uint192(secondsPerLiquidityCumulativesDelta) << 32));
+    }
+
+    /// @notice Given a tick and a token amount, calculates the amount of token received in exchange
+    /// @param tick Tick value used to calculate the quote
+    /// @param baseAmount Amount of token to be converted
+    /// @param baseToken Address of an ERC20 token contract used as the baseAmount denomination
+    /// @param quoteToken Address of an ERC20 token contract used as the quoteAmount denomination
+    /// @return quoteAmount Amount of quoteToken received for baseAmount of baseToken
+    function getQuoteAtTick(
+        int24 tick,
+        uint128 baseAmount,
+        address baseToken,
+        address quoteToken
+    ) internal pure returns (uint256 quoteAmount) {
+        uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(tick);
+
+        // Calculate quoteAmount with better precision if it doesn't overflow when multiplied by itself
+        if (sqrtRatioX96 <= type(uint128).max) {
+            uint256 ratioX192 = uint256(sqrtRatioX96) * sqrtRatioX96;
+            quoteAmount = baseToken < quoteToken
+                ? FixedPointMathLib.mulDivDown(ratioX192, baseAmount, 1 << 192)
+                : FixedPointMathLib.mulDivDown(1 << 192, baseAmount, ratioX192);
+        } else {
+            uint256 ratioX128 = FixedPointMathLib.mulDivDown(sqrtRatioX96, sqrtRatioX96, 1 << 64);
+            quoteAmount = baseToken < quoteToken
+                ? FixedPointMathLib.mulDivDown(ratioX128, baseAmount, 1 << 128)
+                : FixedPointMathLib.mulDivDown(1 << 128, baseAmount, ratioX128);
+        }
+    }
+}
