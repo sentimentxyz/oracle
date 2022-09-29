ba693386c621951c8f9116cbde3cea2b0adb1145
r0ohafza <coldroohafza@gmail.com>
2022-09-29 12:07:50 -0700

    feat: generalize uni v2 LP oracle

--- src/uniswap/UniV2LPOracle.sol
+++ src/uniswap/UniV2LPOracle.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {IERC20} from "../utils/IERC20.sol";
 import {IOracle} from "../core/IOracle.sol";
 import {IUniswapV2Pair} from "./IUniswapV2Pair.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
@@ -37,14 +38,25 @@ contract UniV2LpOracle is IOracle {
     /// @dev Adapted from https://blog.alphaventuredao.io/fair-lp-token-pricing
     /// @inheritdoc IOracle
     function getPrice(address pair) external view returns (uint) {
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+
+        uint decimalSum = IERC20(token0).decimals() + IERC20(token1).decimals();
+
+        uint scale;
+        if (decimalSum > 18) scale =  decimalSum - 18;
+
         (uint r0, uint r1,) = IUniswapV2Pair(pair).getReserves();
 
         // 2 * sqrt(r0 * r1 * p0 * p1) / totalSupply
         return FixedPointMathLib.sqrt(
             r0
-            .mulWadDown(r1)
-            .mulWadDown(oracle.getPrice(IUniswapV2Pair(pair).token0()))
-            .mulWadDown(oracle.getPrice(IUniswapV2Pair(pair).token1()))
+            .mulDivDown(r1, 10 ** scale)
+            .mulDivDown(
+                oracle.getPrice(token0),
+                (scale == 0 ? 10 ** decimalSum : 1e18)
+            )
+            .mulWadDown(oracle.getPrice(token1))
         )
         .mulDivDown(2e27, IUniswapV2Pair(pair).totalSupply());
     }
