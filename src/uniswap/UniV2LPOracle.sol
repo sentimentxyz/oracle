7f548bd0cc51c1bf4e30594ab47acd2611246ed1
Saif Kotwal <saifkotwal2697@gmail.com>
2022-10-10 11:17:51 -0700

    fix: scale reserves

--- src/uniswap/UniV2LPOracle.sol
+++ src/uniswap/UniV2LPOracle.sol
@@ -5,6 +5,7 @@ import {IERC20} from "../utils/IERC20.sol";
 import {IOracle} from "../core/IOracle.sol";
 import {IUniswapV2Pair} from "./IUniswapV2Pair.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+
 /**
     @title Uniswap v2 LP Oracle
     @notice Price oracle for uniswap v2 LP Tokens
@@ -41,23 +42,14 @@ contract UniV2LpOracle is IOracle {
         address token0 = IUniswapV2Pair(pair).token0();
         address token1 = IUniswapV2Pair(pair).token1();
 
-        uint decimalSum = IERC20(token0).decimals() + IERC20(token1).decimals();
-
-        uint scale;
-        if (decimalSum > 18) scale =  decimalSum - 18;
-
         (uint r0, uint r1,) = IUniswapV2Pair(pair).getReserves();
 
         // 2 * sqrt(r0 * r1 * p0 * p1) / totalSupply
         return FixedPointMathLib.sqrt(
-            r0
-            .mulDivDown(r1, 10 ** scale)
-            .mulDivDown(
-                oracle.getPrice(token0),
-                (scale == 0 ? 10 ** decimalSum : 1e18)
-            )
+            (r0 * 10 ** (18 - IERC20(token0).decimals()))
+            .mulWadDown(r1 * 10 ** (18 - IERC20(token1).decimals()))
+            .mulWadDown(oracle.getPrice(token0))
             .mulWadDown(oracle.getPrice(token1))
-        )
-        .mulDivDown(2e27, IUniswapV2Pair(pair).totalSupply());
+        ).mulDivDown(2e27, IUniswapV2Pair(pair).totalSupply());
     }
 }
