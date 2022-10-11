383704dba3058df5456099eda609c0878bdc886d
r0ohafza <coldroohafza@gmail.com>
2022-10-11 16:12:26 -0700

    fix: scale reserves to 18 decimals

--- src/uniswap/UniV2LPOracle.sol
+++ src/uniswap/UniV2LPOracle.sol
@@ -5,6 +5,7 @@ import {IERC20} from "../utils/IERC20.sol";
 import {IOracle} from "../core/IOracle.sol";
 import {IUniswapV2Pair} from "./IUniswapV2Pair.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import "forge-std/console.sol";
 
 /**
     @title Uniswap v2 LP Oracle
@@ -44,10 +45,21 @@ contract UniV2LpOracle is IOracle {
 
         (uint r0, uint r1,) = IUniswapV2Pair(pair).getReserves();
 
+        uint decimals0 = IERC20(token0).decimals();
+        uint decimals1 = IERC20(token1).decimals();
+
+        if (decimals0 <= 18)
+            r0 = r0 * 10 ** (18 - decimals0);
+        else r0 = r0 / 10 ** (decimals0 - 18);
+
+        if (decimals1 <= 18)
+            r1 = r1 * 10 ** (18 - decimals1);
+        else r1 = r1 / 10 ** (decimals1 - 18);
+
         // 2 * sqrt(r0 * r1 * p0 * p1) / totalSupply
         return FixedPointMathLib.sqrt(
-            (r0 * 10 ** (18 - IERC20(token0).decimals()))
-            .mulWadDown(r1 * 10 ** (18 - IERC20(token1).decimals()))
+            r0
+            .mulWadDown(r1)
             .mulWadDown(oracle.getPrice(token0))
             .mulWadDown(oracle.getPrice(token1))
         ).mulDivDown(2e27, IUniswapV2Pair(pair).totalSupply());
