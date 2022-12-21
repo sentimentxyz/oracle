d5fc205a3dd882606e10cef690091771ca7c9252
Rooh Afza <96720500+r0ohafza@users.noreply.github.com>
2022-12-21 11:16:39 -0800

    update tricrypto oracle (#50)

--- src/curve/CurveTriCryptoOracle.sol
+++ src/curve/CurveTriCryptoOracle.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {Errors} from "../utils/Errors.sol";
 import {IOracle} from "../core/IOracle.sol";
+import {AggregatorV3Interface} from "../chainlink/AggregatorV3Interface.sol";
 
 interface ICurveTriCryptoOracle {
     function lp_price() external view returns (uint256);
-}
-
-interface ICurvePool {
-    function price_oracle(uint256) external view returns (uint256);
 }
 
 /**
@@ -25,8 +23,8 @@ contract CurveTriCryptoOracle is IOracle {
     // https://twitter.com/curvefinance/status/1441538795493478415
     ICurveTriCryptoOracle immutable curveTriCryptoOracle;
 
-    /// @notice curve tri crypto pool
-    ICurvePool immutable pool;
+    /// @notice ETH USD Chainlink price feed
+    AggregatorV3Interface immutable ethUsdPriceFeed;
 
     /* -------------------------------------------------------------------------- */
     /*                                 CONSTRUCTOR                                */
@@ -35,11 +33,14 @@ contract CurveTriCryptoOracle is IOracle {
     /**
         @notice Contract constructor
         @param _curveTriCryptoOracle curve tri crypto price oracle
-        @param _pool curve tri crypto pool
+        @param _feed eth/usd feed
     */
-    constructor(ICurveTriCryptoOracle _curveTriCryptoOracle, ICurvePool _pool) {
+    constructor(
+        ICurveTriCryptoOracle _curveTriCryptoOracle,
+        AggregatorV3Interface _feed
+    ) {
         curveTriCryptoOracle = _curveTriCryptoOracle;
-        pool = _pool;
+        ethUsdPriceFeed = _feed;
     }
 
     /* -------------------------------------------------------------------------- */
@@ -47,8 +48,20 @@ contract CurveTriCryptoOracle is IOracle {
     /* -------------------------------------------------------------------------- */
 
     /// @inheritdoc IOracle
-    // pool.price_oracle(1) returns price of WETH
     function getPrice(address) external view returns (uint) {
-        return curveTriCryptoOracle.lp_price() * 1e18 / pool.price_oracle(1);
+        return curveTriCryptoOracle.lp_price() * 1e8 / getEthPrice();
+    }
+
+    function getEthPrice() internal view returns (uint) {
+        (, int answer,, uint updatedAt,) =
+            ethUsdPriceFeed.latestRoundData();
+
+        if (block.timestamp - updatedAt >= 86400)
+            revert Errors.StalePrice(address(0), address(ethUsdPriceFeed));
+
+        if (answer <= 0)
+            revert Errors.NegativePrice(address(0), address(ethUsdPriceFeed));
+
+        return uint(answer);
     }
 }
